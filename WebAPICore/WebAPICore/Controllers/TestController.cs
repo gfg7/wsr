7a8847334404805d8ae16d78082d263bb5466cb6
Юлия Наконечная <nakonechnaya2002@inbox.ru>
2021-10-17 13:29:27 +0500

    basic example

---
+++ WebAPICore/WebAPICore/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPICore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private static readonly Random random = new Random();
+        private static int max = 0;
+
+        [Route("int")]
+        public IActionResult GetInt()
+        {
+            if (max > 0)
+                return Ok(random.Next(0, max));
+            else return BadRequest("set max first");
+        }
+
+        [HttpPost, Route("setmax")]
+        public void SetMax(int max = int.MaxValue)
+        {
+            TestController.max = max;
+        }
+    }
+}
